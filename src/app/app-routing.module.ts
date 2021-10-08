c12a59f78f664aaebc637332d99dce01debdf9b6
C Helder <christian.helder@itvitaelearning.nl>
2021-10-08 15:47:19 +0200

    vrijdag

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', pathMatch: 'full', component: AppComponent},
+  // { path: '/posts', component: PostsComponent}
+  {
+    path: '', component: PostsComponent
+  },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
